52047af6c95ed4da2be2037efd585037545cd255
Cristopher Pozas <cformandoy@gmail.com>
2024-04-21 19:23:10 -0400

    form en proceso

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useForm } from "react-hook-form";
+
+export interface FormData {
+  panel: {
+    largo: number;
+    ancho: number;
+  };
+  techo: {
+    largo: number;
+    ancho: number;
+  };
+}
+
 
 export default function Home() {
+
+  const {handleSubmit} = useForm<FormData>();
+
+
+  const onSubmit = (data: FormData) => {
+    console.log(data)
+  }
   return(
     <>
       <div className="grid grid-cols-4 gap-4" style={{ height: '100vh', padding: '1rem' }}>
 
       <div className="col-span-1">
-        Form
+        <Card style={{ width:'100%', height:'100%'}}>
+          <CardHeader>
+            <CardTitle>titulo</CardTitle>
+            <CardDescription>desc</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Button type="submit" style={{width:'100%'}}>Calcular</Button>
+            </form>
+          </CardContent>
+        </Card>
+
       </div>
       <div className="col-span-3">
         Techo Paneles
